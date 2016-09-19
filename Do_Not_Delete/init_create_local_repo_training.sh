1898567506f4025ed0c74fe2f53046277cc1c74e
GoAsh <ashish.gokal@core.co.za>
2016-09-19 11:35:35 +0200

    Create init_create_local_repo_training.sh

---
+++ Do_Not_Delete/init_create_local_repo_training.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+# install XCode Command Line Tools
+/usr/bin/touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
+PROD=$(softwareupdate -l |
+  grep "\*.*Command Line" |
+  head -n 1 | awk -F"*" '{print $2}' |
+  sed -e 's/^ *//' |
+  tr -d '\n')
+softwareupdate -i "$PROD" -v;
+#Create coretraining folder in Shared folder
+mkdir -p /Shared\ Items/coretraining
+# Clone the iStore-Training Repo into Library
+git clone https://github.com/core-group/iStoresTraining.git /Shared\ Items/coretraining
+# Copy LaunchDaemon
+sudo cp /Shared\ Items/coretraining/com.core.istore.run_training_gitpull.plist /Library/LaunchDaemons/
+launchctl load -w /Library/LaunchDaemons/com.core.istore.run_training_gitpull.plist
