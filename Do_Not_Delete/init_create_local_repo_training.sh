b0f887f4e8fd2e0dfac0f99330d477ff46830277
GoAsh <ashish.gokal@core.co.za>
2016-09-19 16:16:23 +0200

    Update init_create_local_repo_training.sh

--- Do_Not_Delete/init_create_local_repo_training.sh
+++ Do_Not_Delete/init_create_local_repo_training.sh
@@ -7,12 +7,15 @@ PROD=$(softwareupdate -l |
   sed -e 's/^ *//' |
   tr -d '\n')
 softwareupdate -i "$PROD" -v;
-#Create coretraining folder in Shared folder
+# Create coretraining folder in Shared folder
 sudo mkdir -pv /Shared\ Items/coretraining
 # Clone the iStore-Training Repo into Library
 sudo git clone https://github.com/core-group/iStoresTraining.git /Shared\ Items/coretraining
-#Create link of coretraining to Web Server
+# Create link of coretraining to Web Server
 sudo ln -fs /Shared\ Items/coretraining/ /Library/Server/Web/Data/Sites/Default/
 # Copy LaunchDaemon
 sudo cp /Shared\ Items/coretraining/com.core.istore.run_training_gitpull.plist /Library/LaunchDaemons/
 sudo launchctl load -w /Library/LaunchDaemons/com.core.istore.run_training_gitpull.plist
+# Move update repo script into Library folder
+sudo mkdir -pv /Library/iStores
+sudo cp /Shared\ Items/coretraining/Do_Not_Delete/Update_Training_Repo.sh /Library/iStores
